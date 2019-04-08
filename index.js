46783ef82e3e2855cb7d27a9ec7a8db63dbc5414
Xinran Che <gracexinran@gmail.com>
2019-04-08 00:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  var n = Math.floor(Math.random(*1000))
+  if (n = 284) {return n + two
 
   // Feel free to move things around!
-  const two = 2
+  const two = 2}
 }
 
 var funkyFunction = function() {
